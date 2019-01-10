fafb02e261f9485fb2a720b65963f19b591ff025
Vilmos Somogyi <vsomogyi@gmail.com>
2019-01-10 14:48:02 +0100

    dummy page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,28 @@ import Container from '../components/container';
 
 export default () => (
   <Container>
-    <h1>Richard Hamming on Luck</h1>
+    <h1>WeDoQA packages</h1>
     <div>
+      <h2>Quality Assurance and Project Management</h2>
       <p>
-        From Richard Hamming’s classic and must-read talk, “
-        <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-          You and Your Research
-        </a>
-        ”.
+        We specialize in Quality Assurance and Project Management and we do it
+        exceptionally. We are an official Selenium Commercial support provider
+        as part of the Selenium Ecosystem
       </p>
-      <blockquote>
-        <p>
-          There is indeed an element of luck, and no, there isn’t. The prepared
-          mind sooner or later finds something important and does it. So yes, it
-          is luck.{' '}
-          <em>
-            The particular thing you do is luck, but that you do something is
-            not.
-          </em>
-        </p>
-      </blockquote>
+      <h2>What we offer</h2>
+      <p>
+        Our services include working with different methodologies like Agile,
+        Scrumm, Traditional. We adapt to any standard or custom methodology used
+        at the partner's end. We offer QA lead, Project management, QA and PM
+        Consulting. We are familiar with following issue tracking systems: Trac,
+        Team Foundation Server, Asana, Basecamp, Unfuddle, Assembla, Bugzilla,
+        Mantis, Lighthouse, ActiveColab, Fogbugz... We can use any other issue
+        tracker/task manager out there.
+      </p>
+      <h2>Team</h2>
+      <p>
+        Meet the people that are going to take your business to the next level.
+      </p>
     </div>
-    <p>Posted April 09, 2011</p>
   </Container>
 );
